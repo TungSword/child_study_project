a92d90120376ed1f27a557e825559359ccca22ab
於晨吉 <635480945@qq.com>
2024-03-13 22:14:19 +0800

    获取输入框焦点

--- child-study/src/components/math/math_calc.vue
+++ child-study/src/components/math/math_calc.vue
@@ -5,7 +5,8 @@
     <span>{{ second }}</span>
     <span>=</span>
     <br>
-    <el-input-number resize="true" size="large" class="result_input" v-model="result" :controls="false"></el-input-number>
+    <el-input-number ref="resultRef" resize="true" size="large" class="result_input" v-model="result"
+                     :controls="false"></el-input-number>
     <br>
     <el-button class="result_button" type="primary" @click="checkResult">{{ buttonName }}</el-button>
     <br>
@@ -25,7 +26,8 @@ export default {
       methods: ["+", "-"],
       refresh: false,
       buttonName: "确 认",
-      errorCount: 0
+      errorCount: 0,
+      successCount: 0,
     }
   },
   mounted() {
@@ -40,8 +42,9 @@ export default {
       this.result = ""
       this.refresh = false;
       this.buttonName = "确 认"
+      this.$refs.resultRef.focus();
     },
-    calcResult(){
+    calcResult() {
       let calcResult = 0;
       if (this.methodCode === 0) {
         calcResult = this.first + this.second;
@@ -51,7 +54,7 @@ export default {
       return calcResult;
     },
     checkResult() {
-      if (this.errorCount >= 3 && !this.refresh){
+      if (this.errorCount >= 3 && !this.refresh) {
         this.result = this.calcResult()
         this.$message.success(`答案是【${this.result}】，继续努力哦！`)
         this.refresh = true
@@ -65,12 +68,23 @@ export default {
       }
       if (this.result === this.calcResult()) {
         this.refresh = true
-        this.$message.success("恭喜你，答对了！")
         this.buttonName = "下一题"
+        this.successCount++;
+        if (this.successCount % 10 === 0) {
+          this.$message.success(`恭喜你，连续答对【${this.successCount}】题了！`)
+        } else {
+          this.$message.success("恭喜你，答对了！")
+        }
       } else {
         this.result = ""
         this.errorCount++;
-        this.$message.error("回答错误。")
+        if (this.successCount >= 10) {
+          this.$message.error("很遗憾连胜失败。")
+        } else {
+          this.$message.error("回答错误。")
+        }
+        this.successCount = 0;
+        this.$refs.resultRef.focus();
       }
     }
   }
@@ -96,7 +110,8 @@ export default {
     font-size: 30px;
   }
 }
-.el-input--large{
+
+.el-input--large {
   font-size: 30px !important;
 }
 
---
+++ child-study/src/components/math/math_multi_table.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="math_multi_table">
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="less">
+
+</style>
