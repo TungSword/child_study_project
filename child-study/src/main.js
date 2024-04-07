559255da0abecd869cd5bf9c0165f35186042ace
TungSword <TungSword_cn@outlook.com>
2024-04-07 10:55:32 +0800

    Update main.js

--- child-study/src/main.js
+++ child-study/src/main.js
@@ -1,12 +1,12 @@
 import "@/assets/main.css"
 import {createApp} from 'vue'
-import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
 import components from "@/components/index.js";
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(ElementPlus)
 app.use(router)
 
 for (const component in components) {
